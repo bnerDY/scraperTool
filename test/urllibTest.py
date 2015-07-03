9a918d52eeb3edf549032ba0fd8b00c292692299
martin <dingxin.yu@uqconnect.edu.au>
2015-07-03 17:15:31 +0800

    urllib2 & error handling

--- test/urllibTest.py
+++ test/urllibTest.py
@@ -3,17 +3,44 @@ __author__ = 'Martin'
 import urllib
 import urllib2
 
-# create header
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = {'User-Agent': user_agent}
 
-values = {'username': "email", 'password': "password"}
-data = urllib.urlencode(values)  # compiling
-url = "http://passport.csdn.net/account/login"
+class Urltest:
+    def __init__(self):
+        # create header
+        self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        self.headers = {'User-Agent': self.user_agent}
+
+        self.values = {'username': "email", 'password': "password"}
+        self.data = urllib.urlencode(self.values)  # compiling
+        self.url = "http://passport.csdn.net/account/login"
+
+    def request(self):
+        request = urllib2.Request(self.url, self.data, self.headers)
+        response = urllib2.urlopen(request)
+        print response.read()
+
+
+class ErrorHandler:
+    def __init__(self):
+        self.req = urllib2.Request('aaa')
+
+    def handle(self):
+        try:
+            response = urllib2.urlopen(self.req)
+        except urllib2.URLError, e:
+            if hasattr(e, 'code'):
+                print 'The server couldn\'t fulfill the request.'
+                print 'Error code: ', e.code
+            elif hasattr(e, 'reason'):
+                print 'We failed to reach a server.'
+                print 'Reason: ', e.reason
+
+        else:
+            print 'No exception was raised.'
 
 
 if __name__ == '__main__':
-    # geturl = url + "?" + data
-    request = urllib2.Request(url, data, headers)
-    response = urllib2.urlopen(request)
-    print response.read()
+    u = Urltest()
+    u.request()
+    e = ErrorHandler()
+    e.handle()
