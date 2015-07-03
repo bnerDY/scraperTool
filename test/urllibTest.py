a92ef2556e0a38d0186dfc8499957d3aebefb4f6
martin <dingxin.yu@uqconnect.edu.au>
2015-07-03 15:12:48 +0800

    no message

--- test/urllibTest.py
+++ test/urllibTest.py
@@ -3,11 +3,17 @@ __author__ = 'Martin'
 import urllib
 import urllib2
 
+# create header
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = {'User-Agent': user_agent}
+
+values = {'username': "email", 'password': "password"}
+data = urllib.urlencode(values)  # compiling
+url = "http://passport.csdn.net/account/login"
+
+
 if __name__ == '__main__':
-    values = {'username': "email", 'password': "password"}
-    data = urllib.urlencode(values)  # compiling
-    url = "http://passport.csdn.net/account/login"
-    geturl = url + "?" + data
-    request = urllib2.Request(geturl)
+    # geturl = url + "?" + data
+    request = urllib2.Request(url, data, headers)
     response = urllib2.urlopen(request)
     print response.read()
