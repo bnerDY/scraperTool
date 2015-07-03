926420859e67cbb54e8166650f57aa75225d2c59
martin <dingxin.yu@uqconnect.edu.au>
2015-07-03 11:21:09 +0800

    urllib & urllib2 test

--- test/libTest.py
+++ test/libTest.py
@@ -1,8 +1,14 @@
+import urllib
 import urllib2
 
-response = urllib2.urlopen("http://www.baidu.com")
-
+values = {}
+values['username'] = "email"
+values['password'] = "password"
+data = urllib.urlencode(values)
+url = "http://passport.csdn.net/account/login"
+geturl = url + "?" + data
+request = urllib2.Request(geturl)
+response = urllib2.urlopen(request)
 print response.read()
-
 
 __author__ = 'Martin'
