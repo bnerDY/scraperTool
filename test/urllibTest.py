174f55db87d4650827d6c24d1b54f49c03179ae1
martin <dingxin.yu@uqconnect.edu.au>
2015-07-03 15:02:09 +0800

    no message

--- test/urllibTest.py
+++ test/urllibTest.py
@@ -5,7 +5,7 @@ import urllib2
 
 if __name__ == '__main__':
     values = {'username': "email", 'password': "password"}
-    data = urllib.urlencode(values)
+    data = urllib.urlencode(values)  # compiling
     url = "http://passport.csdn.net/account/login"
     geturl = url + "?" + data
     request = urllib2.Request(geturl)
