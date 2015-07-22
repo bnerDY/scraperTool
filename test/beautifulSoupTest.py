66cf5f22f871617e1bb19ce39e3c30212b8e7822
martin <dingxin.yu@uqconnect.edu.au>
2015-07-22 20:32:55 +0800

    html parser lib

---
+++ test/beautifulSoupTest.py
@@ -0,0 +1,25 @@
+__author__ = 'Martin'
+
+from bs4 import BeautifulSoup
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc)
+
+if __name__ == '__main__':
+    print soup.head()
+    for key in soup.find_all('a'):
+        print key.get('class'), key.get("href")
+    print soup.findAll('a')
